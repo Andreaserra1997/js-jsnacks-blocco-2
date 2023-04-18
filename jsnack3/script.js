fb5dcb60444931a00575bb47a48e44377fdc0e7d
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-18 16:38:06 +0200

    console.log sumArr

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,8 +1,10 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 const arr = [0, 1, 2, 3, 4, 5, 6]
+let sumArr = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    if () {
-        
+    if (i % 2 == 1) {
+        sumArr = sumArr + arr[i];
     }
-}
+};
+console.log(sumArr)
